2673963d144ffc65f0fa7ec7763ebf16fe61cf24
JunniorDionisioCasas <i1720115@continental.edu.pe>
2023-01-05 00:46:36 -0500

    Update route for storage link

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 use App\Http\Controllers\ProductoController;
 use App\Http\Controllers\MarcaController;
 use App\Http\Controllers\CategoriaController;
