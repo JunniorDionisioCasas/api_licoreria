ab0d39b6c12cac6dcb045f284049c69028061eba
JunniorDionisioCasas <i1720115@continental.edu.pe>
2023-01-05 00:56:07 -0500

    Update symlink try 2

--- routes/api.php
+++ routes/api.php
@@ -136,3 +136,9 @@ Route::group(['middleware' => ["auth:sanctum"]], function(){
 Route::get('/symlink', function () {
     Artisan::call('storage:link');
 });
+
+Route::get('/link', function () {        
+    $target = '/api.licoreriasansebastian.com/storage/app/public/invoices';
+    $shortcut = '/public_html/api_public/public/storage/invoices';
+    symlink($target, $shortcut);
+ });
