f1707df5ca7f6596db33f41bb1cbd717f69861f8
JunniorDionisioCasas <i1720115@continental.edu.pe>
2023-01-19 16:04:42 -0500

    Update admin home view api

--- app/Http/Controllers/ReportesController.php
+++ app/Http/Controllers/ReportesController.php
@@ -18,23 +18,45 @@ class ReportesController extends Controller
 {
     public function admin_home_info()
     {
-        $weekDays = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
-        $today = Carbon::now('-05:00');
-        $todayDay = $today->isoFormat('d');
-        $barChartLabels = [];
-        for($i=0; $i<7; $i++){
-            $weekIndex = $todayDay + $i + 1;
-            if($weekIndex>6) $weekIndex = $weekIndex - 7;
-            $barChartLabels[$i] = $weekDays[$weekIndex];
-        }
-
         $barChartData = Pedido::selectRaw('SUBSTRING(pdd_fecha_entrega, 1, 10) AS pdd_fecha,
+                                            DAYNAME(pdd_fecha_entrega) as nombre_dia,
+                                            DAYOFWEEK(pdd_fecha_entrega) as dia_de_semana,
                                             COALESCE(SUM(pdd_total), 0) AS total_dia,
                                             COUNT(id_pedido) as num_ventas_dia')
                             ->whereBetween('pdd_fecha_entrega', [Carbon::now('-05:00')->subDays(6)->format('Y-m-d 00:00:00'), Carbon::now('-05:00')->format('Y-m-d 23:59:59')])
-                            ->groupByRaw('SUBSTRING(pdd_fecha_entrega, 0, 10), pdd_fecha')
+                            ->groupByRaw('SUBSTRING(pdd_fecha_entrega, 0, 10), pdd_fecha, nombre_dia, dia_de_semana')
                             ->take(7)
+                            ->orderBy('pdd_fecha', 'asc')
                             ->get();
+        
+        $reorderedBarChartData = [];
+        $weekDays = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
+        $todayDay = Carbon::now('-05:00')->isoFormat('d');
+        for($i=0; $i<7; $i++){
+            $weekIndex = $todayDay + $i + 1;
+            if($weekIndex>6) $weekIndex = $weekIndex - 7;
+
+            $findIndex = null;
+            foreach($barChartData as $index => $d){
+                if($d->dia_de_semana == $weekIndex+1){
+                    $findIndex = $index;
+                    break;
+                }
+            }
+            
+            if ($findIndex !== null){
+                $barChartData[$findIndex]->nombre_dia = $weekDays[$weekIndex];;
+                $reorderedBarChartData[$i] = $barChartData[$findIndex];
+            } else {
+                $noSales = new \stdClass;
+                $noSales->pdd_fecha = Carbon::now('-05:00')->subDays(6-$i)->format('Y-m-d');
+                $noSales->nombre_dia = $weekDays[$weekIndex];
+                $noSales->dia_de_semana = $weekIndex + 1;
+                $noSales->total_dia = 0.0;
+                $noSales->num_ventas_dia = 0;
+                $reorderedBarChartData[$i] = $noSales;
+            };
+        }
         
         $pieData = Detalle_pedido::join('productos', 'detalles_pedidos.id_producto', 'productos.id_producto')
                                 ->selectRaw('productos.prd_nombre, SUM(detalles_pedidos.dtl_cantidad) AS cant_vendidos')
@@ -42,8 +64,7 @@ class ReportesController extends Controller
                                 ->get();
 
         $home_data = new \stdClass;
-        $home_data->barChartLabels = $barChartLabels;
-        $home_data->barChartData = $barChartData;
+        $home_data->barChartData = $reorderedBarChartData;
         $home_data->pieData = $pieData;
         
         return $home_data;
